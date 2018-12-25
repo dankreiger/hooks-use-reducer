4fa16799eff55d93494140c4f3964e0e327f0cd2
Dan Kreiger <dan@dankreiger.com>
2018-12-25 02:15:06 +0100

    add hooks useReducer example

---
+++ src/context.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TodosContext = React.createContext({
+  todos: [
+    { id: 1, text: 'Walk the dog', complete: false },
+    { id: 2, text: 'Change my personality', complete: false },
+    { id: 3, text: 'Learn machine learning', complete: true }
+  ],
+  currentTodo: {}
+});
+
+export default TodosContext;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useContext, useReducer } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
+import TodosContext from './context';
+import todosReducer from './reducer';
+
+const App = () => {
+  const initialState = useContext(TodosContext);
+  const [state, dispatch] = useReducer(todosReducer, initialState);
+  return (
+    <TodosContext.Provider value={{ state, dispatch }}>
+      <TodoForm />
+      <TodoList />
+    </TodosContext.Provider>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
